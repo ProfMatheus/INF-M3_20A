e956c3776c2625fda43f503797b50ac0a816f89e
Max Walter <maxteu100@gmail.com>
2020-07-24 15:12:46 -0300

    um bonequinho

---
+++ Projeto INF M3 20 A/Assets/MaxW/Codigos/Camera1pessoa.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 
+/// Esse codigo tem como utilidade rotacionar 
+/// no eixo "Z" na camera em primeira pessoa.
+/// 
+/// </summary>
+
+public class Camera1pessoa : MonoBehaviour
+{
+    //Essa variavel do tipo float leva o valor da sensibilidade(ajustavel dentro da unity)
+    //esta presente na mutiplicação ao obter a posição do mouse no exioY
+    public      float       sensibilidade   =   2f;
+
+    //Variavel Publica do tipo Float
+    //
+    public      float       mouseY          =   0f;
+
+    void Update()
+    {
+        mouseY = mouseY + (Input.GetAxis("Mouse Y") * sensibilidade);
+        if(mouseY > 60)
+        {
+            mouseY = 60f;
+        }
+        if (mouseY < -55)
+        {
+            mouseY = -55f;
+        }
+        transform.localEulerAngles = new Vector3(x: 0,y: 0,z: mouseY);
+    }
+    
+}
---
+++ Projeto INF M3 20 A/Assets/MaxW/Codigos/Partesdocorpo.cs
@@ -0,0 +1,62 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using TMPro;
+using UnityEngine;
+
+public class Partesdocorpo : MonoBehaviour
+{
+    private Transform parte;
+    private Transform corpo;
+
+    public int eixoX;
+    public int eixoY;
+    public int eixoZ;
+
+    public int rotacaoZ;
+    private bool inversor;
+
+    private float[] cronometro = new float[3];
+
+    public void Start()
+    {
+        parte = GetComponent<Transform>();
+        corpo = GameObject.FindWithTag("Corpo").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cronometro[0] = Time.fixedTime;
+
+        Direito();
+    }
+    public void Direito()
+    {
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
+        {
+            if (inversor == false && (cronometro[1] < cronometro[0]))
+            {
+                parte.transform.localPosition = (new Vector3(x: corpo.localPosition.x - eixoX, y: corpo.localPosition.y - eixoY, z: corpo.localPosition.z + eixoZ));
+                parte.transform.localEulerAngles = new Vector3(x: corpo.localEulerAngles.x, y: corpo.localEulerAngles.y, z: corpo.localEulerAngles.z - rotacaoZ);
+
+                cronometro[2] = cronometro[0] + 0.3f;
+                inversor = true;
+            }
+            else if (inversor == true && (cronometro[2] < cronometro[0]))
+            {
+                parte.transform.localPosition = (new Vector3(x: corpo.localPosition.x + eixoX, y: corpo.localPosition.y - eixoY, z: corpo.localPosition.z + eixoZ));
+                parte.transform.localEulerAngles = new Vector3(x: corpo.localEulerAngles.x, y: corpo.localEulerAngles.y, z: corpo.localEulerAngles.z + rotacaoZ);
+
+                cronometro[1] = cronometro[0] + 0.3f;
+                inversor = false;
+            }
+        }
+        else
+        {
+            parte.transform.localPosition = (new Vector3(x: corpo.localPosition.x, y: corpo.localPosition.y - eixoY, z: corpo.localPosition.z + eixoZ));
+            parte.transform.localEulerAngles = new Vector3(x: corpo.localEulerAngles.x, y: corpo.localEulerAngles.y, z: corpo.localEulerAngles.z);
+        }
+    }
+}
