0b676ccc4da89586ef5711e489ff4ea93de04d6a
lucasmallmannh <mallmannlucas@yahoo.com.br>
2020-07-26 00:34:59 -0300

    Lucas - StatusPlayer
    Acrescentado script de status do personagem. Faltam validações para saber se é o seu player e para setar se foi pego ou não.

---
+++ Projeto INF M3 20 A/Assets/Lucas/StatusPlayer.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Experimental.Rendering;
+using UnityEngine.Experimental.UIElements;
+
+public class StatusPlayer : MonoBehaviour
+{
+    /// <summary>
+    /// Variável para guardar o nome do personagem
+    /// </summary>
+    string nome = "";
+
+    /// <summary>
+    /// Variável para guardar valor da velocidade normal do personagem
+    /// </summary>
+    float velocidade;
+
+    /// <summary>
+    /// Velocidade para guardar a velocidade máxima ao fazer com que o personagem corra
+    /// </summary>
+    float velocidadeCorrer;
+
+    /// <summary>
+    /// Variável para validar se personagem está correndo
+    /// </summary>
+    private bool ativarCorrer = false;
+
+    /// <summary>
+    /// Variável para controlar qual é o seu personagem
+    /// </summary>
+    bool meuJogador = false;
+
+    /// <summary>
+    /// Variável para validar se personagem está pegando ou fugindo
+    /// Se false = fugindo
+    /// Se true = pegando
+    /// </summary>
+    bool pegador = false;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        /// <summary>
+        /// Encontrar uma forma de validar se é o seu jogador, através da cor
+        /// </summary>
+        /*if ()
+        {
+            meuJogador = true;
+        }
+        */
+
+
+
+
+
+        /// <summary>
+        /// Quando apertar a tecla shift (da esquerda), a variável receberá valor true
+        /// </summary>
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            ativarCorrer = true;
+        }
+
+        /// <summary>
+        /// Quando ativarCorrer for true
+        /// a velocidade subirá gradativamente 
+        /// até receber o valor de velocidadeCorrer,
+        /// durante um Time.deltaTime
+        /// </summary>
+        if (ativarCorrer == true)
+        {
+            velocidade = Mathf.Lerp(velocidade, velocidadeCorrer, Time.deltaTime);
+        }
+    }
+}
